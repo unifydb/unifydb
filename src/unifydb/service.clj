b1f680f075e6a2f366f566006dcc6d6f5a34f2fe
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2019-09-09 09:39:57 -0400

    Adopt pubsub model in service

--- src/unifydb/service.clj
+++ src/unifydb/service.clj
@@ -1,29 +1,25 @@
 (ns unifydb.service
-  (:require [unifydb.messagequeue :as queue]))
+  (:require [unifydb.messagequeue :as queue]
+            [unifydb.util :as util]))
 
 (defprotocol IService
   (start! [self])
   (stop! [self]))
 
-(defn consume-loop! [queue-backend queue-name queue-fn started?]
-  "Starts a loop that calls `queue-fn` on every message from `queue-name`
-   while the `started?` function evaluates to true. This function blocks."
-  (loop [message (queue/consume queue-backend queue-name)]
-    (when (started?)
-      (queue-fn @message)
-      (recur (queue/consume queue-backend queue-name)))))
-
 (defrecord QueueConsumerService [queue-backend queue-fns state]
   IService
   (start! [self]
-    ;; For each mapping in queue-fns, start a loop consuming from that queue
     (swap! (:state self) #(assoc %1 :started true))
-    (let [started? (fn [] (:started @(:state self)))]
-     (doseq [[queue-name queue-fn] (:queue-fns self)]
-       (future
-         (consume-loop! (:queue-backend self) queue-name queue-fn started?)))))
+    (doseq [[queue-name queue-fn] (:queue-fns self)]
+      (let [subscription
+            (queue/subscribe (:queue-backend self) queue-name queue-fn)]
+        (swap! (:state self)
+               #(assoc %1 :subscriptions (conj (:subscriptions %1) subscription))))))
   (stop! [self]
+    (doseq [subscription (:subscriptions @(:state self))]
+      (queue/unsubscribe (:queue-backend self) subscription))
     (swap! (:state self) #(assoc %1 :started false))))
 
 (defn make-service [queue-backend queue-fns]
-  (->QueueConsumerService queue-backend queue-fns (atom {:started false})))
+  (->QueueConsumerService queue-backend queue-fns (atom {:started false
+                                                         :subscriptions []})))
