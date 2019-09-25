f6d13a99d4ff2c1079b0b055a76f520a662f9e1b
Jeremy Dormitzer <jeremydormitzer@lola.com>
2019-09-25 09:04:29 -0400

    Add a bunch of repl utils to run the whole system

--- src/unifydb/repl.clj
+++ src/unifydb/repl.clj
@@ -1,12 +1,24 @@
 (ns unifydb.repl
-  (:require [clojure.repl :refer :all]
+  "A bunch of utilities to make the REPL development experience easier"
+  (:require [clojure.edn :as edn]
+            [clojure.repl :refer :all]
+            [unifydb.messagequeue :as queue :refer [publish subscribe]]
             [unifydb.messagequeue.memory :as memq]
+            [unifydb.query :as query]
             [unifydb.service :as service]
-            [unifydb.server :as server]))
+            [unifydb.server :as server]
+            [unifydb.storage.memory :as memstore]
+            [unifydb.transact :as transact]))
 
 (defonce queue (atom (memq/new)))
 
-(defonce server (atom (server/new @queue)))
+(defonce storage (atom (memstore/new)))
+
+(defonce server (atom (server/new @queue @storage)))
+
+(defonce query-service (atom (query/new @queue)))
+
+(defonce transact-service (atom (transact/new @queue)))
 
 (defn start-server! []
   (service/start! @server))
@@ -17,3 +29,64 @@
 (defn restart-server! []
   (service/stop! @server)
   (service/start! @server))
+
+(defn start-query-service! []
+  (service/start! @query-service))
+
+(defn stop-query-service! []
+  (service/stop! @query-service))
+
+(defn restart-query-service! []
+  (service/stop! @query-service)
+  (service/start! @query-service))
+
+(defn start-transact-service! []
+  (service/start! @transact-service))
+
+(defn stop-transact-service! []
+  (service/stop! @transact-service))
+
+(defn restart-transact-service! []
+  (service/stop! @transact-service)
+  (service/start! @transact-service))
+
+(defn start-system! []
+  (start-query-service!)
+  (start-transact-service!)
+  (start-server!))
+
+(defn stop-system! []
+  (stop-query-service!)
+  (stop-transact-service!)
+  (stop-server!))
+
+(defn restart-system! []
+  (restart-query-service!)
+  (restart-transact-service!)
+  (restart-server!))
+
+(defn transact! [tx-data]
+  "Publishes a new transaction to the queue"
+  ;; TODO storage and queue arent' serializable...
+  ;;   will be a problem with non-in-memory queue backends
+  (publish @queue :transact {:conn {:storage-backend @storage
+                                    :queue-backend @queue}
+                             :tx-data tx-data}))
+
+(defn make-request [request]
+  (let [app (server/app @queue @storage)
+        response (promise)
+        respond (fn [r] (deliver response r))
+        raise (fn [e] (throw e))]
+    (app request respond raise)
+    @response))
+
+(defn query [q tx-id]
+  (let [request {:headers {"content-type" "application/edn"
+                           "accept" "application/edn"}
+                 :body (pr-str {:query q
+                                :tx-id tx-id})
+                 :uri "/query"
+                 :request-method :post}
+        response (make-request request)]
+    (edn/read-string (:body response))))
