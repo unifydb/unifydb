fa3ee4370efcc90077b73c2d8c2052e5cf641919
Jeremy Dormitzer <jeremydormitzer@lola.com>
2019-09-26 10:20:11 -0400

    Add TODO comment about flaky test

--- tests/unifydb/server_test.clj
+++ tests/unifydb/server_test.clj
@@ -52,6 +52,7 @@
    :headers {"content-type" "application/json"
              "accept" "application/json"}})
 
+;; TODO this test seemingly-randomly sometimes just hangs forever. Investigate!
 (defservertest query-endpoint [make-request]
   '[[[:unifydb/add "ben" :name "Ben Bitdiddle"]
      [:unifydb/add "ben" :job ["computer" "wizard"]]
