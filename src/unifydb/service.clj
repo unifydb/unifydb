d23baff9304983f195f2c4412ad0f7e1bbe57ffa
Jeremy Dormitzer <jeremydormitzer@lola.com>
2019-11-05 08:04:58 -0500

    Remove unused QueueConsumerService record

--- src/unifydb/service.clj
+++ src/unifydb/service.clj
@@ -7,22 +7,3 @@
 (defprotocol IService
   (start! [self])
   (stop! [self]))
-
-(defrecord QueueConsumerService [queue-backend queue-fns group-name state]
-  IService
-  (start! [self]
-    (swap! (:state self) #(assoc %1 :started true))
-    (doseq [[queue-name queue-fn] (:queue-fns self)]
-      (let [subscription
-            (queue/subscribe (:queue-backend self) queue-name group-name)]
-        (swap! (:state self)
-               #(assoc %1 :subscriptions (conj (:subscriptions %1) subscription)))
-        (s/consume queue-fn subscription))))
-  (stop! [self]
-    (doseq [subscription (:subscriptions @(:state self))]
-      (s/close! subscription))
-    (swap! (:state self) #(assoc %1 :started false))))
-
-(defn make-service [queue-backend queue-fns group-name]
-  (->QueueConsumerService queue-backend queue-fns group-name (atom {:started false
-                                                                    :subscriptions []})))
