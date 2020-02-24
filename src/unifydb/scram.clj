65ce8d1893230276d28589d2a096bd75341b0768
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2020-02-24 07:17:51 -0500

    Don't return hex strings

--- src/unifydb/scram.clj
+++ src/unifydb/scram.clj
@@ -3,12 +3,6 @@
   (:import [javax.crypto Mac]
            [javax.crypto.spec SecretKeySpec]))
 
-(defn hex-string
-  "Returns the hexadecimal representation of `byte-array`
-  as a string"
-  [byte-array]
-  (string/join (map #(format "%x" %) byte-array)))
-
 (defn hmac
   "Calculates the HMAC signature of `string`
   given the `key`. Returns a hex string."
@@ -16,25 +10,26 @@
   (let [mac (Mac/getInstance "HMACSHA256")
         secret (SecretKeySpec. (.getBytes key)
                                (.getAlgorithm mac))]
-    (->> (doto mac
-           (.init secret)
-           (.update (.getBytes string)))
-         (.doFinal)
-         (hex-string))))
+    (-> (doto mac
+          (.init secret)
+          (.update (.getBytes string)))
+        (.doFinal)
+        (String. "UTF-8"))))
 
-;; TODO is this anywhere close?...
 (defn hi
   "The Hi operation defined in RFC5802.
   See https://tools.ietf.org/html/rfc5802."
   [string salt i]
-  (hex-string
-   (apply map bit-xor
-          (map #(.getBytes %)
-               (reduce (fn [acc _]
-                         (conj acc
-                               (hmac string
-                                     (str salt
-                                          (or (last acc)
-                                              (str (char 1)))))))
-                       []
-                       (range i))))))
+  (-> (->>
+       (reduce (fn [acc _]
+                 (conj acc
+                       (hmac string
+                             (str salt
+                                  (or (last acc)
+                                      (str (char 1)))))))
+               []
+               (range i))
+       (map #(.getBytes %))
+       (apply map bit-xor))
+      (byte-array)
+      (String. "UTF-8")))
