f39a81af83f8b4e3705b14ce525863f4af96e8e8
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2020-02-29 11:12:52 -0500

    Operate on raw byte arrays rather than strings

--- src/unifydb/scram.clj
+++ src/unifydb/scram.clj
@@ -5,31 +5,47 @@
 
 (defn hmac
   "Calculates the HMAC signature of `string`
-  given the `key`. Returns a hex string."
+  given the `key`. `string` and `key` should be
+  byte arrays. Returns a byte array."
   [key string]
   (let [mac (Mac/getInstance "HMACSHA256")
-        secret (SecretKeySpec. (.getBytes key)
+        secret (SecretKeySpec. key
                                (.getAlgorithm mac))]
     (-> (doto mac
           (.init secret)
-          (.update (.getBytes string)))
-        (.doFinal)
-        (String. "UTF-8"))))
+          (.update string))
+        (.doFinal))))
+
+(defn bit-xor-array
+  "Computes the piecewise exclusive-or of the input byte arrays.
+  Returns the result as a byte-array.
+
+  All input byte arrays must be the same length."
+  [arr1 arr2 & arrs]
+  (let [len (count arr1)
+        res (byte-array len)]
+    (doseq [arr (into arrs [arr2 arr1])
+            i (range len)]
+      (aset-byte res i (bit-xor (nth res i) (nth arr i))))
+    res))
 
 (defn hi
   "The Hi operation defined in RFC5802.
   See https://tools.ietf.org/html/rfc5802."
   [string salt i]
-  (-> (->>
-       (reduce (fn [acc _]
-                 (conj acc
-                       (hmac string
-                             (str salt
-                                  (or (last acc)
-                                      (str (char 1)))))))
-               []
-               (range i))
-       (map #(.getBytes %))
-       (apply map bit-xor))
-      (byte-array)
-      (String. "UTF-8")))
+  (let [arrs (reduce
+              (fn [acc _]
+                (conj acc
+                      (hmac string
+                            (or (last acc)
+                                (byte-array
+                                 (apply concat
+                                        [salt
+                                         ;; 1 as a 4-byte int:
+                                         [0 0 0 0 0 0 0 0
+                                          0 0 0 0 0 0 0 0
+                                          0 0 0 0 0 0 0 0
+                                          0 0 0 0 0 0 0 1]]))))))
+              []
+              (range i))]
+    (apply bit-xor-array arrs)))
