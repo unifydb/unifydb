91551f65af29c3b29a2d2c813ccf81ccec721d1b
Jeremy Dormitzer <jeremydormitzer@lola.com>
2020-06-07 08:22:22 -0400

    Remove unused function

--- src/unifydb/user.clj
+++ src/unifydb/user.clj
@@ -1,6 +1,5 @@
 (ns unifydb.user
-  (:require [unifydb.scram :as s]
-            [unifydb.transact :as t])
+  (:require [unifydb.scram :as s])
   (:import [java.security SecureRandom]))
 
 (defn make-user
@@ -23,22 +22,3 @@
       :unifydb/server-key (s/encode server-key)
       :unifydb/stored-key (s/encode stored-key)}))
   ([username password] (make-user username password (SecureRandom.))))
-
-(defn new-user!
-  "Persists a new user record to the database.
-  Returns a Manifold deferred with the tx-report.
-  For deterministic output, pass a pre-seeded Random instance."
-  ([queue-backend username password random]
-   (let [{:unifydb/keys [username salt i server-key stored-key]}
-         (make-user username password random)
-         ;; TODO when the transact engine supports using maps directly,
-         ;;      refactor this to just use the user map
-         ;; TODO when unique schema is supported, make :unifydb/username unique
-         tx-data [[:unifydb/add "unifydb/user" :unifydb/username username]
-                  [:unifydb/add "unifydb/user" :unifydb/salt salt]
-                  [:unifydb/add "unifydb/user" :unifydb/i i]
-                  [:unifydb/add "unifydb/user" :unifydb/server-key server-key]
-                  [:unifydb/add "unifydb/user" :unifydb/stored-key stored-key]]]
-     (t/transact queue-backend tx-data)))
-  ([queue-backend username password]
-   (new-user! queue-backend username password (SecureRandom.))))
--- test/unifydb/user_test.clj
+++ test/unifydb/user_test.clj
@@ -1,41 +1,19 @@
 (ns unifydb.user-test
-  (:require [unifydb.user :as u]
-            [unifydb.transact :as transact]
-            [unifydb.messagequeue.memory :as memq]
-            [unifydb.service :as service]
-            [unifydb.storage.memory :as memstore]
-            [clojure.set :refer [subset?]]
-            [clojure.test :refer [deftest is testing]])
+  (:require [clojure.test :refer [deftest is testing]]
+            [unifydb.user :as u])
   (:import [java.util Random]))
 
-(deftest test-new-user!
-  (let [queue (memq/new)
-        transact-service (transact/new queue (memstore/new))
-        seeded-rand (Random. 12345)]
-    (try
-      (service/start! transact-service)
-      (testing "Valid username and password"
-        (let [{{:keys [tx-data tempids]} :tx-report}
-              @(u/new-user! queue "user" "pencil" seeded-rand)
-              user-id (tempids "unifydb/user")
-              tx-id (tempids "unifydb.tx")]
-          (is (subset?
-               #{[user-id :unifydb/username "user" tx-id true]
-                 [user-id :unifydb/salt "1iCfXDGzYYMiqdjueAfI6g==" tx-id true]
-                 [user-id :unifydb/i 4096 tx-id true]
-                 [user-id
-                  :unifydb/server-key
-                  "lJAkDYrYIBgXilqFrA7i9D72FlwWTJXZhYTD0O7JulU="
-                  tx-id
-                  true]
-                 [user-id
-                  :unifydb/stored-key
-                  "SVBCFA/4M6QOwY+UWn5mQ00bsWkZnJrT9CrLzYgrXuA="
-                  tx-id
-                  true]}
-               (set tx-data)))))
-      (testing "Invalid password (fails normalization)"
-        (is (thrown-with-msg? Exception #"Prohibited codepoints"
-                              @(u/new-user! queue "user" "\u0007"))))
-      (finally
-        (service/stop! transact-service)))))
+(deftest test-make-user
+  (let [seeded-rand (Random. 12345)]
+    (testing "Valid username and password"
+      (is (= (u/make-user "user" "pencil" seeded-rand)
+             {:unifydb/username "user"
+              :unifydb/salt "1iCfXDGzYYMiqdjueAfI6g=="
+              :unifydb/i 4096
+              :unifydb/server-key
+              "lJAkDYrYIBgXilqFrA7i9D72FlwWTJXZhYTD0O7JulU="
+              :unifydb/stored-key
+              "SVBCFA/4M6QOwY+UWn5mQ00bsWkZnJrT9CrLzYgrXuA="})))
+    (testing "Invalid password (fails normalization)"
+      (is (thrown-with-msg? Exception #"Prohibited codepoints"
+                            (u/make-user "user" "\u0007"))))))
