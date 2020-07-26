a54ef02c0940d482c0ded0cda6b33181440e657f
Jeremy Dormitzer <jeremydormitzer@lola.com>
2020-07-26 09:10:35 -0400

    Add tests for map->auth-fields, strip-sasl-prefix and list->map

--- test/unifydb/http_auth_test.clj
+++ test/unifydb/http_auth_test.clj
@@ -38,3 +38,38 @@
              (frequencies (map keyword the-keys))))
       (is (= (frequencies (vals parsed))
              (frequencies the-vals))))))
+
+(deftest map->auth-fields
+  (checking "that map->auth-fields produces a SASL header" 100
+    [v (gen/map (gen/fmap keyword auth-field-str)
+                auth-field-str)]
+    (let [fields-str (auth/map->auth-fields v)]
+      (is (= (count v) (get (frequencies fields-str) \= 0)))
+      (is (= (max (- (count v) 1) 0) (get (frequencies fields-str) \, 0)))
+      (is (every? #(s/includes? fields-str (name %)) (keys v)))
+      (is (every? #(s/includes? fields-str %) (vals v))))))
+
+(deftest strip-sasl-prefix
+  (checking "that strip-sasl-prefix strips the prefix" 100
+    [v (gen/let [prefix (gen/such-that
+                         (partial not-includes? " ")
+                         gen/string-alphanumeric)
+                 suffix gen/string-alphanumeric]
+         (gen/return {:suffix suffix
+                      :string (str prefix " " suffix)}))]
+    (let [{:keys [suffix string]} v
+          stripped (auth/strip-sasl-prefix string)]
+      (is (= stripped suffix)))))
+
+(deftest list->map
+  (checking "that list->map turns its arguments into a map" 100
+    [v (gen/let [l (gen/vector gen/any)
+                 ks (gen/vector-distinct gen/keyword
+                                         {:min-elements (count l)
+                                          :max-elements (+ (count l) 20)})]
+         (gen/return [l ks]))]
+    (let [[l ks] v
+          m (auth/list->map ks l)]
+      (is (= (count m) (count l)))
+      (is (= (frequencies (keys m)) (frequencies (take (count l) ks))))
+      (is (= (frequencies (vals m)) (frequencies l))))))
