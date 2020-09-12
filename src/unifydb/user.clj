a1d910ae25afabb90074b885b89b8a893b8fb033
Jeremy Dormitzer <jeremydormitzer@lola.com>
2020-09-12 13:19:43 -0400

    [WIP] Delete scram-reliant user creation

--- src/unifydb/user.clj
+++ src/unifydb/user.clj
@@ -1,5 +1,4 @@
 (ns unifydb.user
-  (:require [unifydb.scram :as s])
   (:import [java.security SecureRandom]))
 
 (defn make-user
@@ -7,18 +6,5 @@
   Does not persist to the database.
   For deterministic output, pass a pre-seeded Random instance."
   ([username password random]
-   (let [salt (s/salt random)
-         i 4096
-         salted-password (s/pbk-df2-hmac-sha256
-                          (.getBytes (s/normalize password))
-                          salt
-                          i)
-         server-key (s/hmac salted-password (.getBytes "Server Key"))
-         client-key (s/hmac salted-password (.getBytes "Client Key"))
-         stored-key (s/hash-sha256 client-key)]
-     {:unifydb/username username
-      :unifydb/salt (s/encode salt)
-      :unifydb/i i
-      :unifydb/server-key (s/encode server-key)
-      :unifydb/stored-key (s/encode stored-key)}))
+   :TODO)
   ([username password] (make-user username password (SecureRandom.))))
