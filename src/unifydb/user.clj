94092ee3d02620438a661b23d437ca059e635e8e
Jeremy Dormitzer <jeremydormitzer@lola.com>
2020-09-13 10:31:16 -0400

    Reimplement make-user to generate salt and hash password

--- src/unifydb/user.clj
+++ src/unifydb/user.clj
@@ -1,4 +1,9 @@
 (ns unifydb.user
+  (:require [buddy.core.hash :as hash]
+            [buddy.core.codecs :as codecs]
+            [buddy.core.bytes :as bytes]
+            [buddy.core.codecs.base64 :as base64]
+            [buddy.core.nonce :as nonce])
   (:import [java.security SecureRandom]))
 
 (defn make-user
@@ -6,5 +11,10 @@
   Does not persist to the database.
   For deterministic output, pass a pre-seeded Random instance."
   ([username password random]
-   :TODO)
+   (let [salt (nonce/random-bytes 64 random)
+         hashed-pw (hash/sha512 (bytes/concat (codecs/str->bytes password)
+                                              salt))]
+     {:unifydb/username username
+      :unifydb/password (codecs/bytes->str (base64/encode hashed-pw))
+      :unifydb/salt (codecs/bytes->str (base64/encode salt))}))
   ([username password] (make-user username password (SecureRandom.))))
