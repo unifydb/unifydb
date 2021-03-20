610d77b2a950019ca625b59e82f6a459dca595fe
Jeremy Dormitzer <jeremydormitzer@lola.com>
2021-03-20 16:36:54 -0400

    Add projectile test command to dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((nil . ((cider-clojure-cli-global-options . "-A:dev")
          (cider-repl-init-code . ("(require 'unifydb.repl) (in-ns 'unifydb.repl)"))
+         (projectile-project-test-cmd . "clojure -A:test")
          (projectile-project-compilation-cmd . "clojure -A:lint"))))
