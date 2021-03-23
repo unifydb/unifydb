8294ef4f01d44ab927b1c04101f0627264896f0a
Jeremy Dormitzer <jeremydormitzer@lola.com>
2021-03-22 21:23:19 -0400

    Get JDBC KV store backend to work for Postgres too

--- src/unifydb/kvstore/jdbc.clj
+++ src/unifydb/kvstore/jdbc.clj
@@ -16,36 +16,43 @@
     [self key]
     (when-let [row (jdbc/execute-one! (:connection self)
                                       ["SELECT value FROM unifydb_kvs WHERE key LIKE ?"
-                                       key])]
+                                       (str key)])]
       (nippy/thaw (:unifydb_kvs/value row))))
   (assoc! [self key val]
     (jdbc/execute! (:connection self) ["BEGIN TRANSACTION"])
     (if (kvstore/contains? self key)
       (jdbc/execute! (:connection self)
-                     ["UPDATE unifydb_kvs SET value = ? WHERE key LIKE ?" (nippy/freeze val) key])
+                     ["UPDATE unifydb_kvs SET value = ? WHERE key LIKE ?"
+                      (nippy/freeze val)
+                      (str key)])
       (jdbc/execute! (:connection self)
-                     ["INSERT INTO unifydb_kvs VALUES (?, ?)" key (nippy/freeze val)]))
+                     ["INSERT INTO unifydb_kvs VALUES (?, ?)"
+                      (str key)
+                      (nippy/freeze val)]))
     (jdbc/execute! (:connection self) ["COMMIT"])
     self)
   (dissoc!
     [self key]
     (jdbc/execute! (:connection self)
                    ["DELETE FROM unifydb_kvs WHERE key LIKE ?"
-                    key])
+                    (str key)])
     self)
   (contains?
     [self key]
     (> (:count (jdbc/execute-one! (:connection self)
                                   ["SELECT COUNT(*) AS count FROM unifydb_kvs WHERE key LIKE ?"
-                                   key]))
+                                   (str key)]))
        0)))
 
 (defn new!
   "Instantiate a new JDBCKeyValueStore using the `connection-uri`,
   creating the unifydb_kvs table if it doesn't exist."
   [connection-uri]
-  (let [conn (jdbc/get-connection connection-uri)]
-    (jdbc/execute! conn ["
-CREATE TABLE IF NOT EXISTS unifydb_kvs (key VARCHAR(255) PRIMARY KEY, value BLOB NOT NULL)
-"])
+  (let [conn (jdbc/get-connection connection-uri)
+        blob-type (if (re-find #"postgresql" connection-uri) "BYTEA" "BLOB")]
+    (jdbc/execute! conn [(format "
+CREATE TABLE IF NOT EXISTS unifydb_kvs (
+    key VARCHAR(255) PRIMARY KEY, value %s NOT NULL
+)
+" blob-type)])
     (->JDBCKeyValueStore conn)))
