70da0504053b242ce905f556b5f5a5cc96052630
Jeremy Dormitzer <jeremydormitzer@lola.com>
2021-04-10 19:41:30 -0400

    Do the demo

--- demo/unifydb/demo.clj
+++ demo/unifydb/demo.clj
@@ -34,6 +34,66 @@
               [:unifydb/add "unifydb.tx" :doc "Initial data load"]])
 
 @(transact (:queue @db-state) tx-data)
+;; => {:tx-report
+;;     {:db-after {:tx-id #unifydb/id 6},
+;;      :tx-data
+;;      [[#unifydb/id 1 :name "Ben Bitdiddle" #unifydb/id 6 true]
+;;       [#unifydb/id 1 :job [:computer :wizard] #unifydb/id 6 true]
+;;       [#unifydb/id 1 :salary 60000 #unifydb/id 6 true]
+;;       [#unifydb/id 2 :name "Alyssa P. Hacker" #unifydb/id 6 true]
+;;       [#unifydb/id 2 :job [:computer :programmer] #unifydb/id 6 true]
+;;       [#unifydb/id 2 :salary 45000 #unifydb/id 6 true]
+;;       [#unifydb/id 2 :supervisor #unifydb/id 1 #unifydb/id 6 true]
+;;       [#unifydb/id 1
+;;        :address
+;;        [:slumerville [:ridge :road] 10]
+;;        #unifydb/id 6
+;;        true]
+;;       [#unifydb/id 2
+;;        :address
+;;        [:cambridge [:mass :ave] 78]
+;;        #unifydb/id 6
+;;        true]
+;;       [#unifydb/id 3 :name "Lem E. Tweakit" #unifydb/id 6 true]
+;;       [#unifydb/id 3 :job [:computer :programmer] #unifydb/id 6 true]
+;;       [#unifydb/id 3 :salary 40000 #unifydb/id 6 true]
+;;       [#unifydb/id 3 :supervisor #unifydb/id 1 #unifydb/id 6 true]
+;;       [#unifydb/id 3
+;;        :address
+;;        [:cambridge [:canal :street] 12]
+;;        #unifydb/id 6
+;;        true]
+;;       [#unifydb/id 4 :name "Louis Reasoner" #unifydb/id 6 true]
+;;       [#unifydb/id 4 :salary 20000 #unifydb/id 6 true]
+;;       [#unifydb/id 4 :job [:chief :intern] #unifydb/id 6 true]
+;;       [#unifydb/id 4 :supervisor #unifydb/id 2 #unifydb/id 6 true]
+;;       [#unifydb/id 4
+;;        :address
+;;        [:slumerville [:davis :square] 42]
+;;        #unifydb/id 6
+;;        true]
+;;       [#unifydb/id 5 :name "Oliver Warbucks" #unifydb/id 6 true]
+;;       [#unifydb/id 5 :job [:chief :executive] #unifydb/id 6 true]
+;;       [#unifydb/id 5 :salary 150000 #unifydb/id 6 true]
+;;       [#unifydb/id 5
+;;        :address
+;;        [:swellesley [:top :heap :road] 1]
+;;        #unifydb/id 6
+;;        true]
+;;       [#unifydb/id 1 :supervisor #unifydb/id 5 #unifydb/id 6 true]
+;;       [#unifydb/id 6 :doc "Initial data load" #unifydb/id 6 true]
+;;       [#unifydb/id 6
+;;        :unifydb/txInstant
+;;        1617925287750
+;;        #unifydb/id 6
+;;        true]],
+;;      :tempids
+;;      {"ben" #unifydb/id 1,
+;;       "alyssa" #unifydb/id 2,
+;;       "lem" #unifydb/id 3,
+;;       "louis" #unifydb/id 4,
+;;       "oliver" #unifydb/id 5,
+;;       "unifydb.tx" #unifydb/id 6}}}
 
 ;; A helper function to query the database
 (defn do-query
@@ -45,6 +105,7 @@
 (do-query '{:find [?e ?salary]
             :where [[?e :name "Ben Bitdiddle"]
                     [?e :salary ?salary]]})
+;; => ([#unifydb/id 1 60000])
 
 ;; Graph query - find backreferences from Ben to those he supervises up to two levels deep
 (do-query '{:find [?employee ?supervisor]
@@ -58,26 +119,37 @@
                       [?e :supervisor ?s]
                       [?s :supervisor ?ben]
                       [?ben :name "Ben Bitdiddle"]]]]})
+;; => (["Alyssa P. Hacker" "Ben Bitdiddle"]
+;;     ["Lem E. Tweakit" "Ben Bitdiddle"]
+;;     ["Louis Reasoner" "Alyssa P. Hacker"])
 
 ;; Unification recurses into collection-type values
 (do-query '{:find [?name ?job-subtype]
             :where [[?e :job [:computer ?job-subtype]]
                     [?e :name ?name]]})
+;; => (["Alyssa P. Hacker" :programmer]
+;;     ["Lem E. Tweakit" :programmer]
+;;     ["Ben Bitdiddle" :wizard])
 
 ;; Destructure lists into first & rest
 (do-query '{:find [?address]
             :where [[_ :address [:slumerville & ?address]]]})
+;; => ([((:davis :square) 42)] [((:ridge :road) 10)])
 
 ;; Query using predicate functions
 (do-query '{:find [?name ?salary]
             :where [[?e :salary ?salary]
                     [(> ?salary 40000)]
                     [?e :name ?name]]})
+;; => (["Alyssa P. Hacker" 45000]
+;;     ["Ben Bitdiddle" 60000]
+;;     ["Oliver Warbucks" 150000])
 
 ;; Predicate functions can be any predicate in clojure.core
 (do-query '{:find [?name]
             :where [[?e :name ?name]
                     [(re-matches #"^L.*" ?name)]]})
+;; => (["Lem E. Tweakit"] ["Louis Reasoner"])
 
 ;; What if Alyssa moves?
 (def move-facts [[:unifydb/retract #unifydb/id 2 :address [:cambridge [:mass :ave] 78]]
@@ -85,6 +157,26 @@
                  [:unifydb/add "unifydb.tx" :doc "Alyssa moved!"]])
 
 @(transact (:queue @db-state) move-facts)
+;; => {:tx-report
+;;     {:db-after {:tx-id #unifydb/id 7},
+;;      :tx-data
+;;      [[#unifydb/id 2
+;;        :address
+;;        [:cambridge [:mass :ave] 78]
+;;        #unifydb/id 7
+;;        false]
+;;       [#unifydb/id 2
+;;        :address
+;;        [:boston [:boylston :street] 112]
+;;        #unifydb/id 7
+;;        true]
+;;       [#unifydb/id 7 :doc "Alyssa moved!" #unifydb/id 7 true]
+;;       [#unifydb/id 7
+;;        :unifydb/txInstant
+;;        1617925849008
+;;        #unifydb/id 7
+;;        true]],
+;;      :tempids {"unifydb.tx" #unifydb/id 7}}}
 
 ;; Which transactions changed Alyssa's address?
 ;; This will be nicer once aggregation operations are added (group by tx-id)
@@ -94,17 +186,28 @@
                     [?tx-id :doc ?doc]]}
           {:tx-id :latest
            :historical true})
+;; => ([#unifydb/id 7 (:cambridge (:mass :ave) 78) false "Alyssa moved!"]
+;;     [#unifydb/id 6
+;;      (:cambridge (:mass :ave) 78)
+;;      true
+;;      "Initial data load"]
+;;     [#unifydb/id 7
+;;      (:boston (:boylston :street) 112)
+;;      true
+;;      "Alyssa moved!"])
 
 ;; We can query using the latest version of the database
 (do-query '{:find [?address]
             :where [[#unifydb/id 2 :address ?address]]}
           ;; equivalent to {:tx-id #unifydb/id 7}
           {:tx-id :latest})
+;; => ([(:boston (:boylston :street) 112)])
 
 ;; Or using a previous transaction as a historical anchor
 (do-query '{:find [?address]
             :where [[#unifydb/id 2 :address ?address]]}
           {:tx-id #unifydb/id 6})
+;; => ([(:cambridge (:mass :ave) 78)])
 
 ;; Rules let us derive new facts from existing ones
 (do-query '{:find [?name]
@@ -116,3 +219,4 @@
                      [?person2 :address [?town & _]]
                      [:not (:same ?person1 ?person2)]]
                     [(:same ?x ?x)]]})
+;; => (["Louis Reasoner"])
